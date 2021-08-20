9212ce63c101e2ab174fc19bbcdd35debf22c168
Jacob Nordan <jacob.nordan113322@gmail.com>
2021-08-20 16:58:44 -0400

    seeing which node modules save

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const inquirer = require('inquirer');
 const mysql2 = require('mysql2');
+
+const viewOrAdd = () => {
+    return inquirer.prompt([{
+            type: 'input',
+            name: 'name',
+            message: 'Manager name?'
+        },
+        {},
+    ])
+}
