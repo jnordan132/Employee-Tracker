0f7205dbd15fed0f0ed58dee9e2ad2b8ff45dd3b
Jacob Nordan <jacob.nordan113322@gmail.com>
2021-08-23 12:55:14 -0400

    created role table

--- seeds.sql
+++ seeds.sql
@@ -1,5 +1,19 @@
-INSERT INTO department (name)
-VALUES ("Sales"),
-       ("Engineering"),
-       ("Finance"),
-       ("Legal");
+-- Department seeds
+INSERT INTO department (department_id, name)
+VALUES (1, "Sales"),
+       (2, "Engineering"),
+       (3, "Finance"),
+       (4, "Legal");
+
+-- Role seeds
+INSERT INTO role (department_id, department, title, salary)
+VALUES (1, "Sales", "Sales Lead", 100000),
+       (1, "Sales", "Salesperson", 80000),
+       (1, "Engineering", "Lead Engineer", 150000),
+       (1, "Engineering", "Software Engineer", 120000),
+       (1, "Finance", "Accountant Manager", 160000),
+       (1, "Finance", "Accountant", 125000),
+       (1, "Legal", "Legal Team Lead", 250000),
+       (1, "Legal", "Lawyer", 190000);
+
+       
