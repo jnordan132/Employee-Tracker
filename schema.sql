6e0b4ff97abf23ca56a8ae5aa2cd7f667e31d15e
Jacob Nordan <jacob.nordan113322@gmail.com>
2021-08-23 12:38:08 -0400

    created layout for employee role table

--- schema.sql
+++ schema.sql
@@ -16,3 +16,14 @@ CREATE TABLE role (
   department_id INT,
   FOREIGN KEY (department_id) REFERENCES department(id)
 );
+
+-- Employee role table
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
