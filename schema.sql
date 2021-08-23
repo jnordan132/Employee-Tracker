c9eec85d3afa97e1ee0db80d8d4f5e80434f3977
Jacob Nordan <jacob.nordan113322@gmail.com>
2021-08-23 12:36:55 -0400

    crated layout for department role table

--- schema.sql
+++ schema.sql
@@ -7,3 +7,12 @@ CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30)
 );
+
+-- Department table
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
