964f637887b5ec3b56aefbb0c6f0de52dac56af4
Jacob Nordan <jacob.nordan113322@gmail.com>
2021-08-22 20:24:17 -0400

    created const connection, connnect function, and startPrompt with list of actions

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
 const inquirer = require('inquirer');
-const mysql2 = require('mysql2');
+const mysql = require('mysql2');
 
-const viewOrAdd = () => {
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Jacob113322",
+    database: "employeeTracker_db"
+});
+
+connection.connect(function(err) {
+    if (err) throw err
+    console.log("Connected as Id" + connection.threadId)
+    startPrompt();
+});
+
+const startPrompt = () => {
     return inquirer.prompt([{
-            type: '',
-            name: '',
-            message: ''
-        },
-        {},
-    ])
-}
+        name: 'database',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: ["View All Departments?", "View All Roles?", "View All Employees?", "Add Department?", "Add Role?", "Add Employee?", "Update Employee Role?"]
+    }]).then(function(val) {
+        switch (val.choice) {
+            case "View All Departments":
+                viewAllDepartments();
+                break;
+
+            case "View All Roles?":
+                viewAllRoles();
+                break;
+
+            case "View All Employees?":
+                viewAllEmployees();
+                break;
+
+            case "Add Department?":
+                addDepartment();
+                break;
+
+            case "Add Role?":
+                addRole();
+                break;
+
+            case "Add Employee?":
+                addEmployee();
+                break;
+
+            case "Update Employee Role?":
+                updateEmployee();
+                break;
+        }
+    })
+};
