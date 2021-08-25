1d8c28ed9f44ce3e6dbc15bacf69fbcdc67b3a52
Jacob Nordan <jacob.nordan113322@gmail.com>
2021-08-25 12:53:14 -0400

    realized we werent running this on server

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
+// Modules needed
 const inquirer = require('inquirer');
-const express = require('express');
 const mysql = require('mysql2');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 // Connection to SQL database
 const db = mysql.createConnection({
@@ -242,7 +236,3 @@ const addRole = () => {
         });
     });
 };
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
