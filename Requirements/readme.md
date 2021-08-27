814f853847f30a061c3f6f5a2ba591b6e17bb3b8
Jacob Nordan <jacob.nordan113322@gmail.com>
2021-08-27 13:23:22 -0400

    Fixed some querys for add functions

--- Requirements/readme.md
+++ Requirements/readme.md
@@ -13,18 +13,17 @@ AS A business owner
 I WANT to be able to view and manage the departments, roles, and employees in my company
 SO THAT I can organize and plan my business
 ```
-
 ## Acceptance Criteria
 
 ```md
 GIVEN a command-line application that accepts user input
 WHEN I start the application
 THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-WHEN I choose to view all departments
+WHEN I choose to view all departments ---------------
 THEN I am presented with a formatted table showing department names and department ids
-WHEN I choose to view all roles
+WHEN I choose to view all roles -------------------
 THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-WHEN I choose to view all employees
+WHEN I choose to view all employees -----------
 THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 WHEN I choose to add a department
 THEN I am prompted to enter the name of the department and that department is added to the database
--- index.js
+++ index.js
@@ -162,7 +162,8 @@ const selectRole = () => {
 // selectManager function for addEmployee prompt
 let managersArr = [];
 const selectManager = () => {
-    db.query("", function(err, res) {
+    const query = `SELECT first_name, last_name FROM employees WHERE manager_id IS NULL`;
+    db.query(query, function(err, res) {
         if (err) throw err
         for (var i = 0; i < res.length; i++) {
             managersArr.push(res[i].first_name);
@@ -171,15 +172,45 @@ const selectManager = () => {
     return managersArr;
 };
 
+// addRole function
+const addRole = () => {
+    const query = `SELECT roles.title, roles.salary FROM roles`;
+    db.query(query, function(err, res) {
+        inquirer.prompt([{
+                name: "Title",
+                type: "input",
+                message: "What is the roles Title?"
+            },
+            {
+                name: "Salary",
+                type: "input",
+                message: "What is the Salary?"
+            }
+        ]).then(function(res) {
+            db.query(
+                "INSERT INTO roles SET ?", {
+                    title: res.Title,
+                    salary: res.Salary,
+                },
+                function(err) {
+                    if (err) throw err
+                    console.table(res);
+                    startPrompt();
+                }
+            )
+        });
+    });
+};
+
 // addEmployee function
 const addEmployee = () => {
     inquirer.prompt([{
-            name: "firstname",
+            name: "first_name",
             type: "input",
             message: "Enter employee first name "
         },
         {
-            name: "lastname",
+            name: "last_name",
             type: "input",
             message: "Enter employee last name "
         },
@@ -198,9 +229,9 @@ const addEmployee = () => {
     ]).then(function(val) {
         const roleId = selectRole().indexOf(val.role) + 1;
         const managerId = selectManager().indexOf(val.choice) + 1;
-        db.query("", {
-            first_name: val.firstName,
-            last_name: val.lastName,
+        db.query("INSERT INTO employees SET ?", {
+            first_name: val.first_name,
+            last_name: val.last_name,
             manager_id: managerId,
             role_id: roleId
         }, function(err) {
@@ -213,7 +244,8 @@ const addEmployee = () => {
 
 // updateEmployee function
 const updateEmployee = () => {
-    db.query("", function(err, res) {
+    const query = `SELECT employees.last_name, employees.role_id FROM employees`;
+    db.query(query, function(err, res) {
         if (err) throw err
         console.log(res)
         inquirer.prompt([{
@@ -248,35 +280,6 @@ const updateEmployee = () => {
                     console.table(val)
                     startPrompt()
                 })
-        });
-    });
-};
-
-// addRole function
-const addRole = () => {
-    db.query("", function(err, res) {
-        inquirer.prompt([{
-                name: "Title",
-                type: "input",
-                message: "What is the roles Title?"
-            },
-            {
-                name: "Salary",
-                type: "input",
-                message: "What is the Salary?"
-            }
-        ]).then(function(res) {
-            db.query(
-                "", {
-                    title: res.Title,
-                    salary: res.Salary,
-                },
-                function(err) {
-                    if (err) throw err
-                    console.table(res);
-                    startPrompt();
-                }
-            )
         });
     });
 };
